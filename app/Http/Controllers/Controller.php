239585641a0bb4e80ce4bcdcb3f09618643ca22a
musickr <13458043813@163.com>
2022-04-02 01:28:13 +0800

    兑换

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -15,12 +15,12 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function success($data = [], $message = 'ok', $code = 1)
+    public function success($data = "", $message = 'ok', $code = 1)
     {
         return response()->json(compact('data', 'message', 'code'),200);
     }
 
-    public function error($message = 'error', $data = [], $code = 0)
+    public function error($message = 'error', $data = "", $code = 0)
     {
         return response()->json(compact('data', 'message', 'code'),400);
     }
