b2db545c3d5bedf40c4a170bd7b62faec324c99d
yzt <1924659848@qq.com>
2022-04-15 10:51:52 +0800

    还原。

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -3,10 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
-//use Laravel\Horizon\Horizon;
-//use Laravel\Horizon\HorizonApplicationServiceProvider;
+use Laravel\Horizon\Horizon;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
 
-class HorizonServiceProvider //extends HorizonApplicationServiceProvider
+class HorizonServiceProvider extends HorizonApplicationServiceProvider
 {
     /**
      * Bootstrap any application services.
@@ -15,7 +15,7 @@ class HorizonServiceProvider //extends HorizonApplicationServiceProvider
      */
     public function boot()
     {
-        //parent::boot();
+        parent::boot();
 
         // Horizon::routeSmsNotificationsTo('15556667777');
         // Horizon::routeMailNotificationsTo('example@example.com');
@@ -39,5 +39,4 @@ class HorizonServiceProvider //extends HorizonApplicationServiceProvider
             ]);
         });
     }
-
 }
