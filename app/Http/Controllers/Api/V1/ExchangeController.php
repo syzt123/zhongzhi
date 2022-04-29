783c373d66ec19811f88ea750740ba061d914156
yzt <1924659848@qq.com>
2022-04-29 11:09:51 +0800

    平台蔬菜新增分类

--- app/Http/Controllers/Api/V1/ExchangeController.php
+++ app/Http/Controllers/Api/V1/ExchangeController.php
@@ -14,6 +14,7 @@ use App\Models\VegetableType;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use function Sodium\compare;
+use Illuminate\Support\Facades\Redis;
 
 class ExchangeController extends Controller
 {
